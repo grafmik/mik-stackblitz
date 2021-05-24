d8bc9f8f79949c08e22151e3c40e5b0d2515f096
grafmik <grafmik@gmail.com>
2021-05-24 20:25:24 +0200

    add mik route

---
+++ pages/mik/[id].js
@@ -0,0 +1,8 @@
+import useRouter from 'next/router';
+
+export default function Mik() {
+  const router = useRouter();
+  const { id } = router.query;
+
+  return <div>mik {id}</div>;
+}
---
+++ pages/mik/index.js
@@ -0,0 +1,3 @@
+export default function Mik() {
+  return <div>mik index</div>;
+}
